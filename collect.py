c847082123b7a2aa37d60ae5be9acf0cab77fd14
maimengmeng <55820563+maimengmeng@users.noreply.github.com>
2025-03-04 23:22:36 +0800

    Update collect.py

--- collect.py
+++ collect.py
@@ -60,10 +60,11 @@ with open(OUTPUT_FILE, 'w', encoding='utf-8') as out_file:
             resp.encoding = resp.apparent_encoding
             
             # 写入文件
-            out_file.write(f"\n\n{'=' * 20}\n")
-            out_file.write(f"# 成功序号: {success_count+1} | 原始序号: {raw_urls.index(url)+1}\n")
-            out_file.write(f"# URL: {url}\n")
-            out_file.write(f"{'=' * 20}\n\n")
+            #out_file.write(f"\n\n{'=' * 20}\n")
+            out_file.write(f"\n\n")
+            #out_file.write(f"# 成功序号: {success_count+1} | 原始序号: {raw_urls.index(url)+1}\n")
+            #out_file.write(f"# URL: {url}\n")
+            #out_file.write(f"{'=' * 20}\n\n")
             out_file.write(resp.text)
             
             success_count += 1
